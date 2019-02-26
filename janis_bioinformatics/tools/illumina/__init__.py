1795689177ff494cc640656b153755671ba0cd12
Michael Franklin <michael@illusional.net>
2019-02-27 09:00:45 +1100

    Ensure Manta is indexed

--- janis_bioinformatics/tools/illumina/__init__.py
+++ janis_bioinformatics/tools/illumina/__init__.py
@@ -1,3 +1,5 @@
 from .strelka.strelka_2_9_9 import Strelka_2_9_9
+from .manta.manta_1_4_0 import Manta_1_4_0
 
 from .strelka.strelka_2_9_9 import StrelkaLatest
+from .manta.manta_1_4_0 import MantaLatest
