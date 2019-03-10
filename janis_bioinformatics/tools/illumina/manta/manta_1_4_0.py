d4d5971f6104a796ad654b6642171eeedc2da1c4
Michael Franklin <michael@illusional.net>
2019-03-11 10:46:04 +1100

    Update Dockers for Manta / Strelka

--- janis_bioinformatics/tools/illumina/manta/manta_1_4_0.py
+++ janis_bioinformatics/tools/illumina/manta/manta_1_4_0.py
@@ -4,11 +4,21 @@ from janis_bioinformatics.tools.illumina.manta.base import MantaBase
 class Manta_1_4_0(MantaBase):
     @staticmethod
     def docker():
-        return "illusional/manta"  # add 1.4.0 after it's released
+        return "michaelfranklin/manta:1.4.0"  # add 1.4.0 after it's released
 
     @staticmethod
     def version():
         return "1.4.0"
 
 
-MantaLatest = Manta_1_4_0
+class Manta_1_5_0(MantaBase):
+    @staticmethod
+    def docker():
+        return "michaelfranklin/manta:1.5.0"  # add 1.4.0 after it's released
+
+    @staticmethod
+    def version():
+        return "1.5.0"
+
+
+MantaLatest = Manta_1_5_0
