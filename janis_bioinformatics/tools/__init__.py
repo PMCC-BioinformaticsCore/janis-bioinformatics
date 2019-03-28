d29857e0e5689a72f0184ba55a27305d676665d6
Michael Franklin <michael@illusional.net>
2019-03-28 16:00:36 +1100

    Add fastqc init file for modules

--- janis_bioinformatics/tools/__init__.py
+++ janis_bioinformatics/tools/__init__.py
@@ -2,7 +2,7 @@
 # import glob
 
 from .bioinformaticstoolbase import BioinformaticsTool, BioinformaticsWorkflow
-from . import bcftools, bwa, common, gatk4, htslib, igvtools, illumina, samtools, validation, babrahambioinformatics
+from . import bcftools, bwa, common, gatk4, htslib, igvtools, illumina, samtools, validation, variantcallers, babrahambioinformatics
 
 # directory = dirname(__file__)
 # modules = glob.glob(directory+"/*")
