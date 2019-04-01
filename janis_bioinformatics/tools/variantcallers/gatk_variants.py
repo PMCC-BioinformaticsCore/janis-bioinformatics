d99f7da226b6633556b249ec78721122a99ab4b6
Michael Franklin <michael@illusional.net>
2019-04-02 09:24:56 +1100

    Add GatkVariant Caller + (omni, hapmap) -> (known, mills) indels

--- janis_bioinformatics/tools/variantcallers/gatk_variants.py
+++ janis_bioinformatics/tools/variantcallers/gatk_variants.py
@@ -1,7 +1,7 @@
-from janis import Step, Input, Directory
+from janis import Step, Input, Directory, Output
 from janis_bioinformatics.tools.common import SplitMultiAllele
 
-from janis_bioinformatics.data_types import FastaWithDict, BamBai, VcfTabix
+from janis_bioinformatics.data_types import FastaWithDict, BamBai, VcfTabix, Bed, VcfIdx
 
 from janis_bioinformatics.tools import BioinformaticsWorkflow
 import janis_bioinformatics.tools.gatk4 as GATK4
@@ -18,12 +18,13 @@ class GatkVariantCaller(BioinformaticsWorkflow):
                                                  doc="GATK4 based variant caller: (BaseRecal + ApplyBQSR + Haplotype)")
 
         bam = Input("bam", BamBai())
+        intervals = Input("intervals", Bed())
         reference = Input("reference", FastaWithDict())
 
         snps_dbsnp = Input("snps_dbsnp", VcfTabix())
         snps_1000gp = Input("snps_1000gp", VcfTabix())
-        omni = Input("omni", VcfTabix())
-        hapmap = Input("hapmap", VcfTabix())
+        known_indels = Input("knownIndels", VcfTabix())
+        mills_indels = Input("millsIndels", VcfTabix())
 
         s1_recal = Step("baseRecalibrator", GATK4.Gatk4BaseRecalibrator_4_0())
         s2_bqsr = Step("applyBQSR", GATK4.Gatk4ApplyBqsr_4_0())
@@ -32,20 +33,27 @@ class GatkVariantCaller(BioinformaticsWorkflow):
 
         # S1: BaseRecalibrator
         self.add_edge(bam, s1_recal.bam)
+        self.add_edge(intervals, s1_recal.intervals)
         self.add_edge(reference, s1_recal.reference)
-        self.add_edge(snps_dbsnp, s1_recal.knownSites)
-        self.add_edge(snps_1000gp, s1_recal.knownSites)
-        self.add_edge(omni, s1_recal.knownSites)
-        self.add_edge(hapmap, s1_recal.knownSites)
+        self.add_edges([
+            (snps_dbsnp, s1_recal.knownSites),
+            (snps_1000gp, s1_recal.knownSites),
+            (known_indels, s1_recal.knownSites),
+            (mills_indels, s1_recal.knownSites),
+        ])
 
         # S2: ApplyBQSR
-        self.add_edge(bam, s2_bqsr.bam)
-        self.add_edge(s1_recal.out, s2_bqsr.recalFile)
-        self.add_edge(reference, s2_bqsr.reference)
+        self.add_edges([
+            (bam, s2_bqsr.bam),
+            (intervals, s2_bqsr.intervals),
+            (s1_recal.out, s2_bqsr.recalFile),
+            (reference, s2_bqsr.reference)
+        ])
 
         # S3: HaplotypeCaller
         self.add_edges([
             (s2_bqsr.out, s3_haplo.inputRead),
+            (intervals, s3_haplo.intervals),
             (reference, s3_haplo.reference),
             (snps_dbsnp, s3_haplo.dbsnp)
         ])
@@ -56,6 +64,9 @@ class GatkVariantCaller(BioinformaticsWorkflow):
             (s3_haplo.out, s4_split.vcf)
         ])
 
+        self.add_edge(s4_split.out, Output("out"))
+
 
 if __name__ == "__main__":
-    print(GatkVariantCaller().dump_translation("cwl"))
+    vc = GatkVariantCaller()
+    # print(vc.dump_translation("cwl"))
