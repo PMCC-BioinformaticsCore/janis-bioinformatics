e9d14821337ecf0127c8c509c5173272afba9dc6
Michael Franklin <michael@illusional.net>
2019-04-11 16:12:24 +1000

    Ensure cutadapt is the python 3 version for multithreading

--- janis_bioinformatics/tools/cutadapt/cutadapt_1_18.py
+++ janis_bioinformatics/tools/cutadapt/cutadapt_1_18.py
@@ -5,4 +5,4 @@ class CutAdapt_1_18(CutAdaptBase):
 
     @staticmethod
     def docker():
-        return "quay.io/biocontainers/cutadapt:1.18--py27h14c3975_1"
+        return "quay.io/biocontainers/cutadapt:1.18--py37h14c3975_1"
