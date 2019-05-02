b4c2aa83a4712d78a65280f7825f50b3b77668b7
Michael Franklin <michael@illusional.net>
2019-05-02 12:52:04 +1000

    Ensure gatherVCFs CL options have prefixes

--- janis_bioinformatics/tools/gatk4/gathervcfs/base.py
+++ janis_bioinformatics/tools/gatk4/gathervcfs/base.py
@@ -21,8 +21,8 @@ class Gatk4GatherVcfsBase(Gatk4ToolBase, ABC):
 
     def inputs(self):
         return [
-            ToolInput("vcfs", Array(Vcf()), doc="[default: []] (-I) Input VCF file(s). "),
-            ToolInput("outputFilename", Filename(extension=".vcf", suffix=".gathered"),
+            ToolInput("vcfs", Array(Vcf()), prefix="--INPUT", doc="[default: []] (-I) Input VCF file(s)."),
+            ToolInput("outputFilename", Filename(extension=".vcf", suffix=".gathered"), prefix="--OUTPUT",
                       doc="[default: null] (-O) Output VCF file."),
             *self.additional_args
         ]
@@ -53,35 +53,37 @@ Input files must be supplied in genomic order and must not have events at overla
         )
 
     additional_args = [
-        ToolInput("argumentsFile", Array(File(), optional=True),
+        ToolInput("argumentsFile", Array(File(), optional=True), prefix="--arguments_file",
                   doc="[default: []] read one or more arguments files and add them to the command line"),
-        ToolInput("compressionLevel", Int(optional=True),
+        ToolInput("compressionLevel", Int(optional=True), prefix="--COMPRESSION_LEVEL",
                   doc="[default: 5] Compression level for all compressed files created (e.g. BAM and VCF)."),
-        ToolInput("createIndex", Boolean(optional=True),
+        ToolInput("createIndex", Boolean(optional=True), prefix="--CREATE_INDEX",
                   doc="[default: TRUE] Whether to create a BAM index when writing a coordinate-sorted BAM file."),
-        ToolInput("createMd5File", Boolean(optional=True),
+        ToolInput("createMd5File", Boolean(optional=True), prefix="--CREATE_MD5_FILE",
                   doc="[default: FALSE] Whether to create an MD5 digest for any BAM or FASTQ files created."),
-        ToolInput("ga4ghClientSecrets", File(optional=True),
+        ToolInput("ga4ghClientSecrets", File(optional=True), prefix="--GA4GH_CLIENT_SECRETS",
                   doc="[default: client_secrets.json] Google Genomics API client_secrets.json file path."),
-        ToolInput("maxRecordsInRam", Int(optional=True),
+        ToolInput("maxRecordsInRam", Int(optional=True), prefix="--MAX_RECORDS_IN_RAM",
                   doc="[default: 500000] When writing files that need to be sorted, this will specify the number of "
                       "records stored in RAM before spilling to disk. Increasing this number reduces the number of "
                       "file handles needed to sort the file, and increases the amount of RAM needed."),
-        ToolInput("quiet", Boolean(optional=True),
+        ToolInput("quiet", Boolean(optional=True), prefix="--QUIET",
                   doc="[default: FALSE] Whether to suppress job-summary info on System.err."),
-        ToolInput("referenceSequence", File(optional=True), doc="[default: null] Reference sequence file."),
-        ToolInput("tmpDir", String(optional=True), default="/tmp",
+        ToolInput("referenceSequence", File(optional=True), prefix="--REFERENCE_SEQUENCE",
+                  doc="[default: null] Reference sequence file."),
+        ToolInput("tmpDir", String(optional=True), default="/tmp", prefix="--TMP_DIR",
                   doc="[default: []] One or more directories with space available to be "
                       "used by this program for temporary storage of working files"),
-        ToolInput("useJdkDeflater", Boolean(optional=True),
+        ToolInput("useJdkDeflater", Boolean(optional=True), prefix="--USE_JDK_DEFLATER",
                   doc="[default: FALSE] (-use_jdk_deflater) Use the JDK Deflater instead "
                       "of the Intel Deflater for writing compressed output"),
-        ToolInput("useJdkInflater", Boolean(optional=True),
+        ToolInput("useJdkInflater", Boolean(optional=True), prefix="--USE_JDK_INFLATER",
                   doc="[default: FALSE] (-use_jdk_inflater) Use the JDK Inflater instead "
                       "of the Intel Inflater for reading compressed input"),
-        ToolInput("validationStringency", String(optional=True),
+        ToolInput("validationStringency", String(optional=True), prefix="--VALIDATION_STRINGENCY",
                   doc="[default: STRICT] Validation stringency for all SAM files read by this program. Setting "
                       "stringency to SILENT can improve performance when processing a BAM file in which "
                       "variable-length data (read, qualities, tags) do not otherwise need to be decoded."),
-        ToolInput("verbosity", Boolean(optional=True), doc="[default: INFO] Control verbosity of logging."),
+        ToolInput("verbosity", Boolean(optional=True), prefix="--VERBOSITY",
+                  doc="[default: INFO] Control verbosity of logging."),
     ]
