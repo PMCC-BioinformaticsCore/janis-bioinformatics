cf518398607f5ae97f8728525ba90ef0b5a91976
Michael Franklin <michael@illusional.net>
2019-06-12 14:18:56 +1000

    Add export of new VariantCallers

--- janis_bioinformatics/tools/variantcallers/__init__.py
+++ janis_bioinformatics/tools/variantcallers/__init__.py
@@ -1,3 +1,3 @@
 from .gatkgermline_variants import GatkGermlineVariantCaller
-from .illuminagermline_strelka import StrelkaGermlineVariantCaller
-from .vardictgermline_variants import VardictVariantCaller
+from .illuminagermline_strelka import IlluminaGermlineVariantCaller
+from .vardictgermline_variants import VardictGermlineVariantCaller
