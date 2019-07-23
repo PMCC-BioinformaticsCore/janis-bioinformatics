cbf424c0e4409e9edcfd885db8f4e9bb91b89b5c
Michael Franklin <michael.franklin@petermac.org>
2019-07-23 14:51:04 +1000

    Add new FastQC version

---
+++ janis_bioinformatics/tools/babrahambioinformatics/fastqc/fastqc_0_11_8.py
@@ -0,0 +1,13 @@
+from janis_bioinformatics.tools.babrahambioinformatics.fastqc.base import FastQCBase
+
+
+class FastQC_0_11_8(FastQCBase):
+
+    @staticmethod
+    def version():
+        return "v0.11.8"
+
+    @staticmethod
+    def docker():
+        return "quay.io/biocontainers/fastqc:0.11.8--1"
+
--- janis_bioinformatics/tools/babrahambioinformatics/fastqc/latest.py
+++ janis_bioinformatics/tools/babrahambioinformatics/fastqc/latest.py
@@ -1,3 +1,3 @@
-from janis_bioinformatics.tools.babrahambioinformatics.fastqc.fastqc_0_11_5 import FastQC_0_11_5
+from janis_bioinformatics.tools.babrahambioinformatics.fastqc.fastqc_0_11_8 import FastQC_0_11_8
 
-FastQCLatest = FastQC_0_11_5
+FastQCLatest = FastQC_0_11_8
