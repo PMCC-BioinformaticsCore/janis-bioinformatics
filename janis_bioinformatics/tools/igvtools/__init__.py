396ffb69c8dcc215f9c8a5d99b42979abb9f1b8b
Michael Franklin <michael.franklin@petermac.org>
2019-07-24 09:38:59 +1000

    Remove unfinished IGVTool

--- janis_bioinformatics/tools/igvtools/__init__.py
+++
@@ -1,2 +0,0 @@
-from .index.index_2_0 import IgvToolsIndex_2_0
-from .index.latest import IgvToolsIndexLatest
--- janis_bioinformatics/tools/igvtools/igvtoolslatest.py
+++
@@ -1,3 +0,0 @@
-from .igvtools_2_0 import IgvTools_2_0
-
-IgvToolsLatest = IgvTools_2_0
