0e1eb5e088596f1f4d9db524ea743ef099980701
Michael Franklin <michael.franklin@petermac.org>
2019-07-23 14:53:52 +1000

    Tag PerformanceValidator

--- janis_bioinformatics/tools/validation/performancevalidator.py
+++ janis_bioinformatics/tools/validation/performancevalidator.py
@@ -1,5 +1,7 @@
+from WDL.Expr import Boolean
+
 from janis_bioinformatics.tools import BioinformaticsWorkflow
-from janis_core import Step, Input, Output, Array
+from janis_core import Step, Input, Output, Array, Boolean
 from janis_bioinformatics.data_types import Vcf, VcfIdx
 from janis_bioinformatics.tools.gatk4 import Gatk4GenotypeConcordanceLatest
 from janis_bioinformatics.tools.htslib import BGZip_1_2_1, Tabix_1_2_1
@@ -15,31 +17,38 @@ class PerformanceValidator_1_2_1(BioinformaticsWorkflow):
         inp_truth = Input("truth", VcfIdx())
         inp_intervals = Input("intervals", Array(Vcf()))
 
-        s1_bgzip = Step("s1_bgzip", BGZip_1_2_1())
-        s2_tabix = Step("s2_tabix", Tabix_1_2_1())
-        s3_genotypeconcord = Step(
-            "s3_genotypeconcord", Gatk4GenotypeConcordanceLatest()
-        )
+        bgzip = Step("bgzip", BGZip_1_2_1())
+        tabix = Step("tabix", Tabix_1_2_1())
+        genotypeconcord = Step("genotypeconcord", Gatk4GenotypeConcordanceLatest())
 
         self.add_edges(
             [
-                (inp, s1_bgzip.file),
-                (s1_bgzip.out, s2_tabix.file),
-                (s2_tabix.out, s3_genotypeconcord.callVCF),
-                (inp_truth, s3_genotypeconcord.truthVCF),
-                (inp_intervals, s3_genotypeconcord.intervals),
+                (inp, bgzip.file),
+                (bgzip.out, tabix.file),
+                (tabix.out, genotypeconcord.callVCF),
+                (inp_truth, genotypeconcord.truthVCF),
+                (inp_intervals, genotypeconcord.intervals),
             ]
         )
 
-        self.add_default_value(s3_genotypeconcord.treatMissingSitesAsHomeRef, True)
+        self.add_edge(Input("treatMissingSitesAsHomeRef", Boolean(), default=True),
+                      genotypeconcord.treatMissingSitesAsHomeRef)
 
         self.add_edges(
             [
-                (s3_genotypeconcord.summaryMetrics, Output("summaryMetrics")),
-                (s3_genotypeconcord.detailMetrics, Output("detailMetrics")),
-                (s3_genotypeconcord.contingencyMetrics, Output("contingencyMetrics")),
+                (genotypeconcord.summaryMetrics, Output("summaryMetrics")),
+                (genotypeconcord.detailMetrics, Output("detailMetrics")),
+                (genotypeconcord.contingencyMetrics, Output("contingencyMetrics")),
             ]
         )
+
+    @staticmethod
+    def version():
+        return "1.2.1"
+
+    @staticmethod
+    def tool_provider():
+        return "Peter MacCallum Cancer Centre"
 
 
 if __name__ == "__main__":
