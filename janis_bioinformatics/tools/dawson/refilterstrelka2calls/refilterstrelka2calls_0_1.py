0d7267f7f2f25d20ae00ae8c219c06a7bc7b8415
SHollizeck <sebastian.hollizeck@gmx.de>
2019-10-10 11:22:34 +1100

    created the dawson toolkit wrapper
    added two tools and their respective wrapper to janis:
    refilterStrelka2Calls
    callSomaticFreeBayes
    Both tools aimed at joint multi sample varinat claling.

---
+++ janis_bioinformatics/tools/dawson/refilterstrelka2calls/refilterstrelka2calls_0_1.py
@@ -0,0 +1,5 @@
+from .base import RefilterStrelka2CallsBase
+from ..dawsontoolkit_0_1 import DawsonToolkit_0_1
+
+class RefilterStrelka2Calls_0_1(RefilterStrelka2CallsBase, DawsonToolkit_0_1):
+    pass
