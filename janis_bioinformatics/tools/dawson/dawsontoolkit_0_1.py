7311857cc146a15a3c80f3ce356387b180c804cc
SHollizeck <sebastian.hollizeck@gmx.de>
2019-10-15 12:13:52 +1100

    update to container 0.1.2

--- janis_bioinformatics/tools/dawson/dawsontoolkit_0_1.py
+++ janis_bioinformatics/tools/dawson/dawsontoolkit_0_1.py
@@ -9,8 +9,8 @@ class DawsonToolkit_0_1(CommandTool, ABC):
 
     @staticmethod
     def container():
-        return "shollizeck/dawsontoolkit:0.1.1"
+        return "shollizeck/dawsontoolkit:0.1.2"
 
     @staticmethod
     def version():
-        return "0.1.1"
+        return "0.1.2"
