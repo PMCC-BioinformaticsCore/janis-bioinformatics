5a501f9959cf0400f3d0e4c806d3e1582752807b
Michael Franklin <michael@illusional.net>
2019-10-18 10:35:57 +1100

    Use versioned tag for freebayes
    This is less likely to change than "latest", may be a stop gap until we build our own managed container.

--- janis_bioinformatics/tools/freebayes/freebayes_1_2.py
+++ janis_bioinformatics/tools/freebayes/freebayes_1_2.py
@@ -4,7 +4,7 @@ from .base import FreeBayesBase
 class FreeBayes_1_2(FreeBayesBase):
     @staticmethod
     def container():
-        return "papaemmelab/docker-freebayes:latest"
+        return "papaemmelab/docker-freebayes:v0.1.5"
 
     @staticmethod
     def version():
