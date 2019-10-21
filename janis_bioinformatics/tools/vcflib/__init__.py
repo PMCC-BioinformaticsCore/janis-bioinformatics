abc371ebeacfb13ef383511fe04dd2a9dd33370e
SebastianHollizeck <sebastian.hollizeck@gmx.de>
2019-10-21 11:52:30 +1100

    changed typo

--- janis_bioinformatics/tools/vcflib/__init__.py
+++ janis_bioinformatics/tools/vcflib/__init__.py
@@ -3,7 +3,7 @@ from .vcfallelicprimitives.versions import (
     VcfAllelicPrimitivesLatest,
 )
 from .vcffixup.versions import VcfFixUp_1_0_1, VcfFixUpLatest
-from .vcfrandomsample.versions import VcfRamdomSample_1_0_1, VcfRandomSampleLatest
+from .vcfrandomsample.versions import VcfRandomSample_1_0_1, VcfRandomSampleLatest
 from .vcfroc.versions import VcfRoc_1_0_1, VcfRocLatest
 from .vcfuniq.versions import VcfUniq_1_0_1, VcfUniqLatest
 from .vcfuniqalleles.versions import VcfUniqAlleles_1_0_1, VcfUniqAllelesLatest
