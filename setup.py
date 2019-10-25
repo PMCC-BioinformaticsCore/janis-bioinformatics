6bfa73f46da9e0364b079153d1c16ca0bc38ff75
Michael Franklin <michael.franklin@petermac.org>
2019-10-25 14:11:36 +1100

    Readd core requirement

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         "janis.tools": ["bioinformatics=janis_bioinformatics.tools"],
         "janis.types": ["bioinformatics=janis_bioinformatics.data_types"],
     },
-    install_requires=["janis-pipelines.core"],
+    install_requires=["janis-pipelines.core >= 0.7.0"],
     zip_safe=False,
     long_description=long_description,
     long_description_content_type="text/markdown",
