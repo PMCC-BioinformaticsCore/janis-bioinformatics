8562a143a31b1f68ef23409204fbbb8be16a1af7
SHollizeck <sebastian.hollizeck@gmx.de>
2019-10-28 12:21:14 +1100

    set to defaultly split along the canonical chromosomes

--- janis_bioinformatics/tools/dawson/workflows/freebayessomaticworkflow.py
+++ janis_bioinformatics/tools/dawson/workflows/freebayessomaticworkflow.py
@@ -63,7 +63,37 @@ class FreeBayesSomaticWorkflow(BioinformaticsWorkflow):
         self.input("bams", Array(BamBai))
 
         self.input("reference", FastaWithDict)
-        self.input("callRegions", Array(String, optional=True))
+        self.input(
+            "callRegions",
+            Array(String, optional=True),
+            default=[
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                "10",
+                "11",
+                "12",
+                "13",
+                "14",
+                "15",
+                "16",
+                "17",
+                "18",
+                "19",
+                "20",
+                "21",
+                "22",
+                "X",
+                "Y",
+                "MT",
+            ],
+        )
 
         self.input("normalSample", String)
         self.input("sampleNames", Array(String, optional=True))
