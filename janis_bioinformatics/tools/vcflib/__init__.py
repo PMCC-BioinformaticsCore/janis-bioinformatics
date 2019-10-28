85f997324b6132b19b9e093dc76efe31f70b8533
SHollizeck <sebastian.hollizeck@gmx.de>
2019-10-28 18:32:31 +1100

    exports setup

--- janis_bioinformatics/tools/vcflib/__init__.py
+++ janis_bioinformatics/tools/vcflib/__init__.py
@@ -8,3 +8,4 @@ from .vcfroc.versions import VcfRoc_1_0_1, VcfRocLatest
 from .vcfuniq.versions import VcfUniq_1_0_1, VcfUniqLatest
 from .vcfuniqalleles.versions import VcfUniqAlleles_1_0_1, VcfUniqAllelesLatest
 from .vcfcombine.versions import VcfCombine_1_0_1, VcfCombineLatest
+from .vcfstreamsort.versions import VcfStreamSort_1_0_1, VcfStreamSortLatest
