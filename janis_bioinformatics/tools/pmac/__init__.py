69243439542d6ca8408bea0d784e94e42c85d5c2
Michael Franklin <michael.franklin@petermac.org>
2020-01-09 11:21:40 +1100

    Add ParseFASTQC (first PythonTool [codetool])

--- janis_bioinformatics/tools/pmac/__init__.py
+++ janis_bioinformatics/tools/pmac/__init__.py
@@ -1,2 +1,6 @@
-from .combinevariants.combinevariants_0_0_4 import CombineVariants_0_0_4, CombineVariantsLatest
+from .combinevariants.combinevariants_0_0_4 import (
+    CombineVariants_0_0_4,
+    CombineVariantsLatest,
+)
 from .trimiupac.trimiupac_0_0_4 import TrimIUPAC_0_0_4, TrimIUPACLatest
+from .parsefastqc.base import ParseFastqcAdaptors
