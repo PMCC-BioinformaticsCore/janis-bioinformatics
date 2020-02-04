0272f171f2ebb2a6e1593f3cfc4e939d171eeb52
SHollizeck <sebastian.hollizeck@gmx.de>
2020-02-04 12:19:32 +1100

    default call regions are now smaller regions to stay below 24h runtime

--- janis_bioinformatics/tools/dawson/workflows/freebayessomaticworkflow.py
+++ janis_bioinformatics/tools/dawson/workflows/freebayessomaticworkflow.py
@@ -71,37 +71,89 @@ class FreeBayesSomaticWorkflow(BioinformaticsWorkflow):
             "callRegions",
             Array(String, optional=True),
             default=[
-                "chr1",
-                "chr2",
-                "chr3",
-                "chr4",
-                "chr5",
-                "chr6",
-                "chr7",
-                "chr8",
-                "chr9",
-                "chr10",
-                "chr11",
-                "chr12",
-                "chr13",
-                "chr14",
-                "chr15",
-                "chr16",
-                "chr17",
-                "chr18",
-                "chr19",
-                "chr20",
-                "chr21",
-                "chr22",
-                "chrX",
-                "chrY",
-                "chrM",
+                "chr1:1-50000000",
+                "chr1:50000000-100000000",
+                "chr1:100000000-150000000",
+                "chr1:150000000-200000000",
+                "chr1:200000000-248956422",
+                "chr2:1-50000000",
+                "chr2:50000000-100000000",
+                "chr2:100000000-150000000",
+                "chr2:150000000-200000000",
+                "chr2:200000000-242193529",
+                "chr3:1-50000000",
+                "chr3:50000000-100000000",
+                "chr3:100000000-150000000",
+                "chr3:150000000-198295559",
+                "chr4:1-50000000",
+                "chr4:50000000-100000000",
+                "chr4:100000000-150000000",
+                "chr4:150000000-190214555",
+                "chr5:1-50000000",
+                "chr5:50000000-100000000",
+                "chr5:100000000-150000000",
+                "chr5:150000000-181538259",
+                "chr6:1-50000000",
+                "chr6:50000000-100000000",
+                "chr6:100000000-150000000",
+                "chr6:150000000-170805979",
+                "chr7:1-50000000",
+                "chr7:50000000-100000000",
+                "chr7:100000000-150000000",
+                "chr7:150000000-159345973",
+                "chr8:1-50000000",
+                "chr8:50000000-100000000",
+                "chr8:100000000-145138636",
+                "chr9:1-50000000",
+                "chr9:50000000-100000000",
+                "chr9:100000000-138394717",
+                "chr10:1-50000000",
+                "chr10:50000000-100000000",
+                "chr10:100000000-133797422",
+                "chr11:1-50000000",
+                "chr11:50000000-100000000",
+                "chr11:100000000-135086622",
+                "chr12:1-50000000",
+                "chr12:50000000-100000000",
+                "chr12:100000000-133275309",
+                "chr13:1-50000000",
+                "chr13:50000000-100000000",
+                "chr13:100000000-114364328",
+                "chr14:1-50000000",
+                "chr14:50000000-100000000",
+                "chr14:100000000-107043718",
+                "chr15:1-50000000",
+                "chr15:50000000-100000000",
+                "chr15:100000000-101991189",
+                "chr16:1-50000000",
+                "chr16:50000000-90338345",
+                "chr17:1-50000000",
+                "chr17:50000000-83257441",
+                "chr18:1-50000000",
+                "chr18:50000000-80373285",
+                "chr19:1-50000000",
+                "chr19:50000000-58617616",
+                "chr20:1-50000000",
+                "chr20:50000000-64444167",
+                "chr21:1-46709983",
+                "chr22:1-50000000",
+                "chr22:50000000-50818468",
+                "chrX:1-50000000",
+                "chrX:50000000-100000000",
+                "chrX:100000000-150000000",
+                "chrX:150000000-156040895",
             ],
         )
 
         self.input("normalSample", String)
         self.input("sampleNames", Array(String, optional=True))
 
+        # for the moment this is a bit wonky, because you need to specify something which is
+        # affected by the amount of bams that you specify (bam coverage just gets summed up at this
+        # location)
+        # so the formula at the moment would be nBams * coverage = skipCov
+        # which means for 8 bams with an average coverage of 160 you would probably want
+        # 8 * 400 = 1600 to be on the save side
         self.input("skipCov", Int(optional=True), default=500)
 
         self.step(
@@ -205,7 +257,10 @@ class FreeBayesSomaticWorkflow(BioinformaticsWorkflow):
 
         self.step("indexFinal", TabixLatest(file=self.compressFinal.out))
 
-        self.output("outVcf", source=self.indexFinal)
+        self.output("somaticOutVcf", source=self.indexFinal)
+        self.output(
+            "variantsOutVcf", source=self.callVariants, output_folder=self.callRegions
+        )
 
 
 if __name__ == "__main__":
