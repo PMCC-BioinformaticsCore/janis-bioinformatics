20d9fc2490a8ed95946d57a1983d43128c2215cb
Matthias De Smet <matthias.desmet@ugent.be>
2020-02-27 13:30:43 +0100

    bamsormadup: fix docker url

--- janis_bioinformatics/tools/biobambam/versions.py
+++ janis_bioinformatics/tools/biobambam/versions.py
@@ -3,7 +3,7 @@ from abc import ABC
 
 class BioBamBam_2_0_87(ABC):
     def container(self):
-        return "quay.io/biocontainers/biobambam:2.0.87-1"
+        return "quay.io/biocontainers/biobambam:2.0.87--1"
 
     def version(self):
         return "2.0.87"
