6ed30529d66e77708b1dacd90d775d19271e8f4d
Jiaan Yu <jiaan7.yu@gmail.com>
2020-04-03 14:13:41 +0800

    Tool performanceSummary add tool metadata

--- janis_bioinformatics/tools/pmac/performancesummary/base.py
+++ janis_bioinformatics/tools/pmac/performancesummary/base.py
@@ -1,4 +1,5 @@
 from abc import ABC
+import datetime
 
 from janis_bioinformatics.tools import BioinformaticsTool
 from janis_core import ToolInput, ToolOutput, File, Filename, InputSelector, Boolean
@@ -65,3 +66,39 @@ class PerformanceSummaryBase(BioinformaticsTool, ABC):
             doc="calculate statistics for whole genome data.--target_flagstat must not be speicified",
         ),
     ]
+
+    def bind_metadata(self):
+        self.metadata.creator = "Jiaan Yu"
+        self.metadata.dateUpdated = datetime.datetime(2020, 4, 3)
+        self.metadata.contributors = ["Jiaan Yu"]
+        self.metadata.documentation = """usage: performance_summary.py [-h] --flagstat FLAGSTAT
+                              --collect_insert_metrics COLLECT_INSERT_METRICS
+                              --coverage COVERAGE -o O
+                              [--target_flagstat TARGET_FLAGSTAT]
+                              [--rmdup_flagstat RMDUP_FLAGSTAT] [--genome]
+
+Performance summary of bam
+
+required arguments:
+  --flagstat FLAGSTAT   output of samtools flagstat on bam
+  --collect_insert_metrics COLLECT_INSERT_METRICS
+                        output of CollectInsertMetrics (GATK or Picard) on bam
+  --coverage COVERAGE   output of bedtools coverageBed for targeted bam;
+                        bedtools genomeCoverageBed for whole genome bam
+  -o O                  output summary csv name
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --target_flagstat TARGET_FLAGSTAT
+                        output of samtools flagstat of bam target on target
+                        bed. Only specified for targeted bam
+  --rmdup_flagstat RMDUP_FLAGSTAT
+                        output of samtools flagstat of removed duplicates bam.
+                        File to be used to extract mapping infomation if
+                        specified, instead of the --flagstat file.
+  --genome              calculate statistics for whole genome data.
+                        --target_flagstat must not be speicified
+        """
+        self.metadata.documentationUrl = (
+            "https://github.com/PMCC-BioinformaticsCore/scripts/tree/master/performance"
+        )
