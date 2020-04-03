dbde14788f3c3ea959919af7c5da28e582c59dfd
Jiaan Yu <jiaan7.yu@gmail.com>
2020-04-03 15:32:25 +0800

    Add new tool geneCoveragePerSample

---
+++ janis_bioinformatics/tools/pmac/genecovpersample/versions.py
@@ -0,0 +1,11 @@
+from janis_bioinformatics.tools.pmac.genecovpersample.base import (
+    GeneCoveragePerSampleBase,
+)
+from janis_bioinformatics.tools.pmac.versions import PeterMacUtils_0_0_6
+
+
+class GeneCoveragePerSample_0_0_6(GeneCoveragePerSampleBase, PeterMacUtils_0_0_6):
+    pass
+
+
+GeneCoveragePerSampleLatest = GeneCoveragePerSample_0_0_6
