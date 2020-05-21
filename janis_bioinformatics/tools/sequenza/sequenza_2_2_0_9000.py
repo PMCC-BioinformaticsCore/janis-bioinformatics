876fe7eb615ab85cd1d76230120b797e0a9c4d3f
Michael Franklin <michael.franklin@petermac.org>
2020-05-22 09:24:08 +1000

    Minor `black` formatting to sequenza files

--- janis_bioinformatics/tools/sequenza/sequenza_2_2_0_9000.py
+++ janis_bioinformatics/tools/sequenza/sequenza_2_2_0_9000.py
@@ -1,6 +1,6 @@
 class Sequenza_2_2_0_9000:
-  def container(self):
-    return "sequenza/sequenza:2.2.0.9000"
+    def container(self):
+        return "sequenza/sequenza:2.2.0.9000"
 
-  def version(self):
-    return "2.2.0.9000"
+    def version(self):
+        return "2.2.0.9000"
