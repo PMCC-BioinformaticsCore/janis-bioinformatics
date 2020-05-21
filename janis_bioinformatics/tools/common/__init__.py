91ead63da628a42f20e7d0ca807da11205907987
Jiaan Yu <jiaan7.yu@gmail.com>
2020-05-21 17:55:56 +0800

    Add subworkflow VcfMergeHeader and VcfConcatContent for strelka somatic

--- janis_bioinformatics/tools/common/__init__.py
+++ janis_bioinformatics/tools/common/__init__.py
@@ -4,3 +4,5 @@ from .mergeandmark.mergeandmark_4_1_3 import MergeAndMarkBams_4_1_3
 from .splitmultiallele import SplitMultiAllele
 from .bwamem_samtoolsview import BwaMem_SamToolsView
 from .indexfasta import IndexFasta
+from .vcf.vcf_merge_header import VcfMergeHeader
+from .vcf.vcf_concat_content import VcfConcatContent
