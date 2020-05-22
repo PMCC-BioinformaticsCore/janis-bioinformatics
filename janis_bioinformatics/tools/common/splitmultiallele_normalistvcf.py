67decf2cbdb2f67d3710707a79488b794e07c548
Jiaan Yu <jiaan7.yu@gmail.com>
2020-05-22 14:33:42 +0800

    Add tool to split mutiallele and normalise vcf

---
+++ janis_bioinformatics/tools/common/splitmultiallele_normalistvcf.py
@@ -0,0 +1,84 @@
+from typing import List
+
+from janis_bioinformatics.data_types import Vcf, FastaWithDict
+from janis_bioinformatics.tools import BioinformaticsTool
+from janis_core import (
+    ToolOutput,
+    ToolInput,
+    Filename,
+    ToolArgument,
+    InputSelector,
+)
+
+
+class SplitMultiAlleleNormaliseVcf(BioinformaticsTool):
+    def tool(self):
+        return "SplitMultiAlleleNormaliseVcf"
+
+    def tool_provider(self):
+        return "Peter MacCallum Cancer Centre"
+
+    def friendly_name(self):
+        return "Split Multiple Alleles and Normalise Vcf"
+
+    def base_command(self):
+        return None
+
+    def container(self):
+        return "heuermh/vt"
+
+    def version(self):
+        return "v0.5772"
+
+    def inputs(self) -> List[ToolInput]:
+        return [
+            ToolInput("vcf", Vcf(), position=1, shell_quote=False),
+            ToolInput(
+                "reference", FastaWithDict(), prefix="-r", position=4, shell_quote=False
+            ),
+            ToolInput(
+                "outputFilename",
+                Filename(prefix=InputSelector("vcf"), extension=".vcf", suffix=".norm"),
+                position=6,
+                prefix="-o",
+                shell_quote=False,
+            ),
+        ]
+
+    def outputs(self) -> List[ToolOutput]:
+        return [ToolOutput("out", Vcf(), glob=InputSelector("outputFilename"))]
+
+    def arguments(self):
+        return [
+            ToolArgument("vt decompose -s ", position=0, shell_quote=False),
+            ToolArgument("| vt normalize -n -q - ", position=2, shell_quote=False),
+        ]
+
+    def doc(self):
+        return """Use vt to split multiallelic variants, and left-most align normalisation.
+        Original command:
+        vt decompose -s $input.vcf | vt normalize -n -q - -r $reference -o $output.vcf
+            VT decompose documentation:
+        options : -s  smart decomposition [false]
+              -d  debug [false]
+              -f  filter expression []
+              -o  output VCF file [-]
+              -I  file containing list of intervals []
+              -i  intervals []
+              -?  displays help
+              
+    VT normalize documentation:
+        options : -o  output VCF file [-]
+              -d  debug [false]
+              -q  do not print options and summary [false]
+              -m  warns but does not exit when REF is inconsistent
+                  with masked reference sequence for non SNPs.
+                  This overides the -n option [false]
+              -n  warns but does not exit when REF is inconsistent
+                  with reference sequence for non SNPs [false]
+              -f  filter expression []
+              -w  window size for local sorting of variants [10000]
+              -I  file containing list of intervals []
+              -i  intervals []
+              -r  reference sequence fasta file []
+              -?  displays help """.strip()
