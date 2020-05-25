7e2f7a6ea175aabe93df02dbec4738fa709ba73d
Jiaan Yu <jiaan7.yu@gmail.com>
2020-05-25 15:31:39 +0800

    Modify vardict germline variant calling

--- janis_bioinformatics/tools/variantcallers/vardictgermline_variants.py
+++ janis_bioinformatics/tools/variantcallers/vardictgermline_variants.py
@@ -3,7 +3,7 @@ from janis_core import File, String, Float
 from janis_bioinformatics.data_types import FastaWithDict, BamBai, Bed
 from janis_bioinformatics.tools import BioinformaticsWorkflow
 from janis_bioinformatics.tools.bcftools import BcfToolsAnnotate_1_5
-from janis_bioinformatics.tools.common import SplitMultiAllele
+from janis_bioinformatics.tools.common import SplitMultiAlleleNormaliseVcf
 from janis_bioinformatics.tools.vardict import VarDictGermline_1_6_0
 from janis_bioinformatics.tools.pmac.trimiupac.versions import TrimIUPAC_0_0_5
 
@@ -51,13 +51,15 @@ class VardictGermlineVariantCaller(BioinformaticsWorkflow):
         )
         self.step(
             "annotate",
-            BcfToolsAnnotate_1_5(file=self.vardict.out, headerLines=self.header_lines),
+            BcfToolsAnnotate_1_5(vcf=self.vardict.out, headerLines=self.header_lines),
         )
         self.step(
-            "split_multi_allele",
-            SplitMultiAllele(vcf=self.annotate.out, reference=self.reference),
+            "splitnormalisevcf",
+            SplitMultiAlleleNormaliseVcf(
+                vcf=self.annotate.out, reference=self.reference
+            ),
         )
-        self.step("trim", TrimIUPAC_0_0_5(vcf=self.split_multi_allele.out))
+        self.step("trim", TrimIUPAC_0_0_5(vcf=self.splitnormalisevcf.out))
 
         self.output("vardict_variants", source=self.vardict.out)
         self.output("out", source=self.trim.out)
