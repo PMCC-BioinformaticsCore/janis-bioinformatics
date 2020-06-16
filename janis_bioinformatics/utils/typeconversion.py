d85e46992050054b214a6850769d8b370e2cba48
Michael Franklin <michael.franklin@petermac.org>
2020-06-17 01:05:05 +1000

    Sebastian drawsontoolkit minorfixes (#45)
    * Update type conversion script with different optional support
    
    * Add id(): super().id() + "_cram" to Cram converted tools

--- janis_bioinformatics/utils/typeconversion.py
+++ janis_bioinformatics/utils/typeconversion.py
@@ -13,17 +13,21 @@ def cast_input_bams_to_crams(inputs):
 
         # we need to check for BamBai first, as due to inheritance, the bambai is also a bam
         if isinstance(inp.input_type, BamBai):
-            inp.input_type = CramCrai()
+            inp.input_type = CramCrai(optional=is_optional)
         elif isinstance(inp.input_type, Bam):
-            inp.input_type = Cram()
-        elif isinstance(inp.input_type, Array) and isinstance(
-            inp.input_type.subtype(), BamBai
-        ):
-            inp.input_type = Array(CramCrai)
-        elif isinstance(inp.input_type, Array) and isinstance(
-            inp.input_type.subtype(), Bam
-        ):
-            inp.input_type = Array(Cram)
+            inp.input_type = Cram(optional=is_optional)
+        elif isinstance(inp.input_type, Array):
+            internal = None
+            if isinstance(inp.input_type.subtype(), BamBai):
+                internal = CramCrai
+            elif isinstance(inp.input_type.subtype(), Bam):
+                internal = Cram
+
+            if internal is not None:
+                inp.input_type = Array(
+                    internal(optional=inp.input_type.subtype().optional),
+                    optional=is_optional,
+                )
 
         # and now that we changed things, we set the optional state again
         inp.input_type.optional = is_optional
