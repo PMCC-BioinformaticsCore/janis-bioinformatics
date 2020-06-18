e5e8373ba3c84c2c1ff59ce6eb640611fa888e5e
Jiaan Yu <jiaan7.yu@gmail.com>
2020-06-18 13:09:31 +0800

    gatk somatic variant calling add tumor/normal bam splitting

--- janis_bioinformatics/tools/variantcallers/gatk/gatksomatic_variants_4_1_3.py
+++ janis_bioinformatics/tools/variantcallers/gatk/gatksomatic_variants_4_1_3.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-from janis_core import String, Array
+from janis_core import String, Array, WorkflowBuilder
 from janis_unix.tools import UncompressArchive
 from janis_bioinformatics.tools import gatk4
 from janis_bioinformatics.data_types import FastaWithDict, BamBai, VcfTabix, Bed
@@ -22,32 +22,35 @@ class GatkSomaticVariantCaller_4_1_3(BioinformaticsWorkflow):
 
     def constructor(self):
 
-        self.input("normal_bam", Array(BamBai(), optional=True))
+        self.input("normal_bam", BamBai)
         self.input("tumor_bam", BamBai)
-
         self.input("normal_name", String(optional=True))
-
         self.input(
             "intervals",
             Bed(optional=True),
             doc="This optional intervals file supports processing by regions. If this file resolves "
             "to null, then GATK will process the whole genome per each tool's spec",
         )
-
         self.input("reference", FastaWithDict)
-        self.input("snps_dbsnp", VcfTabix)
-        self.input("snps_1000gp", VcfTabix)
-        self.input("known_indels", VcfTabix)
-        self.input("mills_indels", VcfTabix)
         self.input("gnomad", VcfTabix)
         self.input("panel_of_normals", VcfTabix(optional=True))
+
+        # split normal and tumor bam
+        self.step(
+            "normal",
+            self.process_subpipeline(bam=self.normal_bam, intervals=self.intervals),
+        )
+        self.step(
+            "tumor",
+            self.process_subpipeline(bam=self.tumor_bam, intervals=self.intervals),
+        )
 
         # variant calling + learn read orientation model
         self.step(
             "mutect2",
             gatk4.GatkMutect2_4_1_3(
-                normalBams=self.normal_bam,
-                tumorBams=self.tumor_bam,
+                normalBams=self.normal.out,
+                tumorBams=self.tumor.out,
                 normalSample=self.normal_name,
                 intervals=self.intervals,
                 reference=self.reference,
@@ -66,7 +69,7 @@ class GatkSomaticVariantCaller_4_1_3(BioinformaticsWorkflow):
         self.step(
             "getpileupsummaries",
             gatk4.Gatk4GetPileUpSummariesLatest(
-                bam=self.tumor_bam, sites=self.gnomad, intervals=self.intervals
+                bam=self.tumor.out, sites=self.gnomad, intervals=self.intervals
             ),
         )
         self.step(
@@ -93,22 +96,32 @@ class GatkSomaticVariantCaller_4_1_3(BioinformaticsWorkflow):
             "splitnormalisevcf",
             SplitMultiAllele(vcf=self.uncompressvcf.out, reference=self.reference),
         )
-        self.step("compressvcf", BGZipLatest(file=self.splitnormalisevcf.out))
-        self.step("tabixvcf", TabixLatest(inp=self.compressvcf.out))
         self.step(
             "filterpass",
             VcfToolsvcftoolsLatest(
-                vcf=self.tabixvcf.out,
+                vcf=self.splitnormalisevcf.out,
                 removeFileteredAll=True,
                 recode=True,
                 recodeINFOAll=True,
             ),
         )
-        self.step("uncompressvcf2", UncompressArchive(file=self.filterpass.out))
 
         self.output("variants", source=self.filtermutect2calls.out)
         self.output("out_bam", source=self.mutect2.bam)
-        self.output("out", source=self.uncompressvcf2.out)
+        self.output("out", source=self.filterpass.out)
+
+    @staticmethod
+    def process_subpipeline(**connections):
+        w = WorkflowBuilder("somatic_subpipeline")
+
+        w.input("bam", BamBai)
+        w.input("intervals", Bed(optional=True))
+        w.step(
+            "split_bam", gatk4.Gatk4SplitReads_4_1_3(bam=w.bam, intervals=w.intervals)
+        )
+        w.output("out", source=w.split_bam.out)
+
+        return w(**connections)
 
     def bind_metadata(self):
         self.metadata.version = "4.1.3.0"
