d6f3de3cdccff2987fefd6c7e7ec22d13419cb1b
Jiaan Yu <jiaan7.yu@gmail.com>
2020-06-25 11:32:21 +0800

    GATK3 and tools add version 3.8.0

--- janis_bioinformatics/tools/gatk3/depthofcoverage/versions.py
+++ janis_bioinformatics/tools/gatk3/depthofcoverage/versions.py
@@ -1,5 +1,9 @@
 from .base import GATK3DepthOfCoverageBase
-from ..versions import GATK3_3_8_1
+from ..versions import GATK3_3_8_0, GATK3_3_8_1
+
+
+class GATK3DepthOfCoverage_3_8_0(GATK3_3_8_0, GATK3DepthOfCoverageBase):
+    pass
 
 
 class GATK3DepthOfCoverage_3_8_1(GATK3_3_8_1, GATK3DepthOfCoverageBase):
