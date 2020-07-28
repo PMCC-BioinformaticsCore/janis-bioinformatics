db95a524e7c056c2519f325fc733e036db546ede
SHollizeck <sebastian.hollizeck@gmx.de>
2020-07-28 19:07:39 +1000

    update to latest docker

--- janis_bioinformatics/tools/dawson/dawsontoolkit_0_1.py
+++ janis_bioinformatics/tools/dawson/dawsontoolkit_0_1.py
@@ -9,8 +9,8 @@ class DawsonToolkit_0_1(CommandTool, ABC):
 
     @staticmethod
     def container():
-        return "shollizeck/dawsontoolkit:0.1.6.1"
+        return "shollizeck/dawsontoolkit:0.1.7"
 
     @staticmethod
     def version():
-        return "0.1.6"
+        return "0.1.7"
