76377ae320f7031cd2dc032b40cdf7d99f0e44e3
Michael Franklin <michael.franklin@petermac.org>
2020-07-30 09:57:34 +1000

    Add auto docstrings to fastqc_single_scattered

--- janis_bioinformatics/tools/babrahambioinformatics/fastqc/versions.py
+++ janis_bioinformatics/tools/babrahambioinformatics/fastqc/versions.py
@@ -1,4 +1,5 @@
-from janis_core import Array, ToolMetadata
+from typing import Dict
+from janis_core import Array, ToolMetadata, ToolInput
 from janis_bioinformatics.data_types import FastqGz
 
 from janis_bioinformatics.tools.bioinformaticstoolbase import BioinformaticsWorkflow
@@ -55,7 +56,7 @@ FastQCSingleLatest = FastQCSingle_0_11_8
 
 ## Single scatter
 
-fastqc_single_version = FastQCSingleLatest().version()
+fastqc_single_instantiated = FastQCSingleLatest()
 
 
 class FastqcSingleScattered(BioinformaticsWorkflow):
@@ -63,28 +64,35 @@ class FastqcSingleScattered(BioinformaticsWorkflow):
     FastQC doesn't return a Directory unless it's the single variant, but Janis will make
     you double scatter if you're processing an array of array of fastqs.
     
-    Note, this is bound to the LATEST version of FastQC: '{fastqc_single_version}'
+    Note, this is bound to the LATEST version of FastQC: '{fastqc_single_instantiated.version()}'
     """
 
     def constructor(self):
         self.input("reads", Array(FastqGz))
 
+        kwargs_to_process = {
+            "casva",
+            "nano",
+            "nofilter",
+            "noextract",
+            "nogroup",
+            "format",
+            "contaminants",
+            "adapters",
+            "limits",
+            "kmers",
+        }
+        tins: Dict[str, ToolInput] = {
+            i.id(): i for i in fastqc_single_instantiated.inputs()
+        }
+        kwargs = {
+            i.id(): self.input(i.id(), i.input_type, doc=i.doc)
+            for i in tins.values()
+            if i.id() in kwargs_to_process
+        }
+
         self.step(
-            "fastqc",
-            FastQCSingleLatest(
-                read=self.reads,
-                casava=None,
-                nano=None,
-                nofilter=None,
-                noextract=None,
-                nogroup=None,
-                format=None,
-                contaminants=None,
-                adapters=None,
-                limits=None,
-                kmers=None,
-            ),
-            scatter="read",
+            "fastqc", FastQCSingleLatest(read=self.reads, **kwargs), scatter="read",
         )
 
         self.capture_outputs_from_step(self.fastqc)
@@ -93,10 +101,10 @@ class FastqcSingleScattered(BioinformaticsWorkflow):
         return "FastQC (Single, Scattered)"
 
     def id(self) -> str:
-        return "fastqc_single_array"
+        return "fastqc_single_scattered"
 
     def version(self):
-        return fastqc_single_version
+        return fastqc_single_instantiated.version()
 
     def bind_metadata(self):
         return ToolMetadata(
@@ -106,6 +114,6 @@ class FastqcSingleScattered(BioinformaticsWorkflow):
 FastQC doesn't return a Directory unless it's the single variant, but Janis will make
 you double scatter if you're processing an array of array of fastqs.
 
-Note, this is bound to the LATEST version of FastQC: '{fastqc_single_version}'
+Note, this is bound to the LATEST version of FastQC: '{fastqc_single_instantiated.version()}'
     """,
         )
