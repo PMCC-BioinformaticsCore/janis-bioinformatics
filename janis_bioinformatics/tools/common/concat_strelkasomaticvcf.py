bc56d49b75d67934f9e30940be28c4ade92cdc33
Jiaan Yu <jiaan7.yu@gmail.com>
2020-07-29 16:42:59 +0800

    Change output to vcf

--- janis_bioinformatics/tools/common/concat_strelkasomaticvcf.py
+++ janis_bioinformatics/tools/common/concat_strelkasomaticvcf.py
@@ -10,7 +10,7 @@ from janis_core import (
     InputSelector,
     String,
 )
-from janis_bioinformatics.data_types import CompressedVcf, VcfTabix
+from janis_bioinformatics.data_types import CompressedVcf, VcfTabix, Vcf
 from janis_bioinformatics.tools.bioinformaticstoolbase import BioinformaticsTool
 
 
@@ -39,7 +39,7 @@ class ConcatStrelkaSomaticVcf(BioinformaticsTool):
             ToolInput("contentVcfs", Array(VcfTabix), position=4),
             ToolInput(
                 "outputFilename",
-                Filename(extension=".vcf.gz", suffix=".strelka"),
+                Filename(extension=".vcf", suffix=".strelka"),
                 prefix=">",
                 position=6,
                 shell_quote=False,
@@ -47,9 +47,7 @@ class ConcatStrelkaSomaticVcf(BioinformaticsTool):
         ]
 
     def outputs(self) -> List[ToolOutput]:
-        return [
-            ToolOutput("out", CompressedVcf(), glob=InputSelector("outputFilename"),)
-        ]
+        return [ToolOutput("out", Vcf(), glob=InputSelector("outputFilename"),)]
 
     def arguments(self):
         return [
@@ -57,16 +55,15 @@ class ConcatStrelkaSomaticVcf(BioinformaticsTool):
             ToolArgument("| grep '^##' > header.vcf;", position=2, shell_quote=False),
             ToolArgument("vcf-concat", position=3, shell_quote=False),
             ToolArgument(
-                "| grep -v '^##' > content.vcf; cat header.vcf content.vcf | bgzip -c ",
+                "| grep -v '^##' > content.vcf; cat header.vcf content.vcf",
                 position=5,
                 shell_quote=False,
             ),
         ]
 
     def doc(self):
-        return """Concat the SNV and INDEL vcfs from strelka somatic variant calling, and compress tabix into one vcf.gz
+        return """Concat the SNV and INDEL vcfs from strelka somatic variant calling
         Command:
         vcf-concat $vcf1.gz $vcf2.gz > header.vcf;
         vcf-merge $vcf1.gz $vcf2.gz > content.vcf;
-        cat header.vcf content.vcf > ${output}.vcf.gz;
-        tabix ${output}.vcf.gz"""
+        cat header.vcf content.vcf > ${output}.vcf;"""
