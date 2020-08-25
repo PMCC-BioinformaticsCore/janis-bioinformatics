fc1987c812e4cd20654b88bc537ce3575775ecb9
SHollizeck <sebastian.hollizeck@gmx.de>
2020-08-25 20:12:54 +1000

    importing and seeting up non cram version as well

--- janis_bioinformatics/tools/gatk4/getpileupsummaries/versions.py
+++ janis_bioinformatics/tools/gatk4/getpileupsummaries/versions.py
@@ -1,6 +1,6 @@
 from .base import Gatk4GetPileUpSummariesBase
 from .base_cram import Gatk4GetPileUpSummariesCramBase
-from ..versions import Gatk_4_1_2_0, Gatk_4_1_3_0, Gatk_4_1_4_0
+from ..versions import Gatk_4_1_2_0, Gatk_4_1_3_0, Gatk_4_1_4_0, Gatk_4_1_6_0
 
 
 class Gatk4GetPileUpSummaries_4_1_2(Gatk_4_1_2_0, Gatk4GetPileUpSummariesBase):
@@ -12,6 +12,10 @@ class Gatk4GetPileUpSummaries_4_1_3(Gatk_4_1_3_0, Gatk4GetPileUpSummariesBase):
 
 
 class Gatk4GetPileUpSummaries_4_1_4(Gatk_4_1_4_0, Gatk4GetPileUpSummariesBase):
+    pass
+
+
+class Gatk4GetPileUpSummaries_4_1_6(Gatk_4_1_6_0, Gatk4GetPileUpSummariesBase):
     pass
 
 
@@ -31,7 +35,7 @@ class Gatk4GetPileUpSummariesCram_4_1_6(Gatk_4_1_6_0, Gatk4GetPileUpSummariesCra
     pass
 
 
-Gatk4GetPileUpSummariesLatest = Gatk4GetPileUpSummaries_4_1_4
+Gatk4GetPileUpSummariesLatest = Gatk4GetPileUpSummaries_4_1_6
 
 if __name__ == "__main__":
     print(Gatk4GetPileUpSummariesBase().help())
--- janis_bioinformatics/tools/gatk4/mutect2/versions.py
+++ janis_bioinformatics/tools/gatk4/mutect2/versions.py
@@ -2,7 +2,13 @@ from .base_4_0 import Gatk4Mutect2Base_4_0
 from .base_4_1 import Gatk4Mutect2Base_4_1
 from .base_4_1_cram import Gatk4Mutect2CramBase_4_1
 
-from ..versions import Gatk_4_0_12, Gatk_4_1_2_0, Gatk_4_1_3_0, Gatk_4_1_4_0
+from ..versions import (
+    Gatk_4_0_12,
+    Gatk_4_1_2_0,
+    Gatk_4_1_3_0,
+    Gatk_4_1_4_0,
+    Gatk_4_1_6_0,
+)
 
 
 class GatkMutect2_4_0(Gatk_4_0_12, Gatk4Mutect2Base_4_0):
@@ -18,6 +24,10 @@ class GatkMutect2_4_1_3(Gatk_4_1_3_0, Gatk4Mutect2Base_4_1):
 
 
 class GatkMutect2_4_1_4(Gatk_4_1_4_0, Gatk4Mutect2Base_4_1):
+    pass
+
+
+class GatkMutect2_4_1_6(Gatk_4_1_6_0, Gatk4Mutect2Base_4_1):
     pass
 
 
@@ -37,7 +47,7 @@ class GatkMutect2Cram_4_1_6(Gatk_4_1_6_0, Gatk4Mutect2CramBase_4_1):
     pass
 
 
-GatkMutect2Latest = GatkMutect2_4_1_4
+GatkMutect2Latest = GatkMutect2_4_1_6
 
 if __name__ == "__main__":
     print(GatkMutect2_4_0().help())
