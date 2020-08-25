bdf1c442e5eb3553c34ebe7eb2534a3ca8e0bbcb
Michael Franklin <michael.franklin@petermac.org>
2020-08-26 08:31:17 +1000

    Add new version of GATK + update haplotypecaller + mutect2
    Probably need an easier way to mass update tools

--- janis_bioinformatics/tools/gatk4/haplotypecaller/versions.py
+++ janis_bioinformatics/tools/gatk4/haplotypecaller/versions.py
@@ -1,5 +1,13 @@
 from .base import Gatk4HaplotypeCallerBase
-from ..versions import Gatk_4_0_12, Gatk_4_1_2_0, Gatk_4_1_3_0, Gatk_4_1_4_0
+from ..versions import (
+    Gatk_4_0_12,
+    Gatk_4_1_2_0,
+    Gatk_4_1_3_0,
+    Gatk_4_1_4_0,
+    Gatk_4_1_6_0,
+    Gatk_4_1_7_0,
+    Gatk_4_1_8_1,
+)
 
 
 class Gatk4HaplotypeCaller_4_0(Gatk_4_0_12, Gatk4HaplotypeCallerBase):
@@ -18,7 +26,19 @@ class Gatk4HaplotypeCaller_4_1_4(Gatk_4_1_4_0, Gatk4HaplotypeCallerBase):
     pass
 
 
-Gatk4HaplotypeCallerLatest = Gatk4HaplotypeCaller_4_1_4
+class Gatk4HaplotypeCaller_4_1_6(Gatk_4_1_6_0, Gatk4HaplotypeCallerBase):
+    pass
+
+
+class Gatk4HaplotypeCaller_4_1_7(Gatk_4_1_7_0, Gatk4HaplotypeCallerBase):
+    pass
+
+
+class Gatk4HaplotypeCaller_4_1_8(Gatk_4_1_8_1, Gatk4HaplotypeCallerBase):
+    pass
+
+
+Gatk4HaplotypeCallerLatest = Gatk4HaplotypeCaller_4_1_8
 
 if __name__ == "__main__":
-    print(Gatk4HaplotypeCaller_4_0().help())
+    print(Gatk4HaplotypeCaller_4_1_8().help())
