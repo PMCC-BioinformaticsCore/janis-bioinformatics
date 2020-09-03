bcef3dfdd414d4a972649028384d43062f16dccb
Michael Franklin <michael.franklin@petermac.org>
2020-09-03 10:58:33 +1000

    Fix arriba version

--- janis_bioinformatics/tools/suhrig/arriba/versions.py
+++ janis_bioinformatics/tools/suhrig/arriba/versions.py
@@ -3,7 +3,7 @@ from .base import ArribaBase
 
 class Arriba_1_2_0(ArribaBase):
     def version(self):
-        return "1.2.0--hd2e4403_2"
+        return "1.2.0"
 
     def container(self):
         return "quay.io/biocontainers/arriba:1.2.0--hd2e4403_2"
