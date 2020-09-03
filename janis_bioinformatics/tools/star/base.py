b7cfba2d14b4a0d75bba17085b986ef0940f17e2
Michael Franklin <michael.franklin@petermac.org>
2020-09-03 10:58:58 +1000

    Fix extra type on STAR base

--- janis_bioinformatics/tools/star/base.py
+++ janis_bioinformatics/tools/star/base.py
@@ -148,7 +148,7 @@ class StarBase(BioinformaticsTool, ABC):
             ),
             ToolInput(
                 "genomeConsensusFile",
-                String(optional=True),
+                Vcf(optional=True),
                 prefix="--genomeConsensusFile",
                 doc="(default: -) VCF file with consensus SNPs (i.e. alternative allele is the major (AF>0.5) allele)",
             ),
