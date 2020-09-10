4877a4d67274c4bb4233ceb3a21d0c9e571529b7
Jiaan Yu <jiaan7.yu@gmail.com>
2020-09-10 11:08:28 +0800

    Add arriba 1.1.0

--- janis_bioinformatics/tools/suhrig/arriba/versions.py
+++ janis_bioinformatics/tools/suhrig/arriba/versions.py
@@ -1,6 +1,14 @@
 from .base import ArribaBase
 
 
+class Arriba_1_1_0(ArribaBase):
+    def version(self):
+        return "1.1.0"
+
+    def container(self):
+        return "quay.io/biocontainers/arriba:1.1.0--h10824c4_1"
+
+
 class Arriba_1_2_0(ArribaBase):
     def version(self):
         return "1.2.0"
