dff6f80d17a5181558075d633fb6ee11373bd7ce
Jiaan Yu <jiaan7.yu@gmail.com>
2020-09-22 15:54:24 +0800

    Gatk4Variantfiltration remove versions before 4.1.4.0

--- janis_bioinformatics/tools/gatk4/reordersam/versions.py
+++ janis_bioinformatics/tools/gatk4/reordersam/versions.py
@@ -1,17 +1,5 @@
 from .base import Gatk4ReorderSamBase
-from ..versions import Gatk_4_0_12, Gatk_4_1_2_0, Gatk_4_1_3_0, Gatk_4_1_4_0
-
-
-class Gatk4ReorderSam_4_0(Gatk_4_0_12, Gatk4ReorderSamBase):
-    pass
-
-
-class Gatk4ReorderSam_4_1_2(Gatk_4_1_2_0, Gatk4ReorderSamBase):
-    pass
-
-
-class Gatk4ReorderSam_4_1_3(Gatk_4_1_3_0, Gatk4ReorderSamBase):
-    pass
+from ..versions import Gatk_4_1_4_0
 
 
 class Gatk4ReorderSam_4_1_4(Gatk_4_1_4_0, Gatk4ReorderSamBase):
@@ -21,4 +9,4 @@ class Gatk4ReorderSam_4_1_4(Gatk_4_1_4_0, Gatk4ReorderSamBase):
 Gatk4ReorderSamLatest = Gatk4ReorderSam_4_1_4
 
 if __name__ == "__main__":
-    print(Gatk4ReorderSam_4_0().help())
+    print(Gatk4ReorderSam_4_1_4().help())
