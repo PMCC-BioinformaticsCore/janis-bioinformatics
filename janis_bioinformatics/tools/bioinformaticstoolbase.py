e44fc095cb6f7cdbc27c41fb0aba76de05cb766a
Juny Kesumadewi <jkesumadewi@5000L-152848-M.local>
2020-10-05 10:06:21 +1100

    Add functions to evaluate tool metadata and translation

--- janis_bioinformatics/tools/bioinformaticstoolbase.py
+++ janis_bioinformatics/tools/bioinformaticstoolbase.py
@@ -1,6 +1,13 @@
+import docker
+import os
 from typing import Dict, Union
 from abc import ABC
-import docker
+
+from janis_core.translationdeps.supportedtranslations import SupportedTranslation
+from janis_core.translations.cwl import CwlTranslator
+from janis_core.translations.wdl import WdlTranslator
+from janis_core.utils.metadata import ToolMetadata
+
 
 from janis_core import (
     CommandTool,
@@ -21,13 +28,37 @@ class BioinformaticsTool(CommandTool, ABC):
         evaluation = {}
 
         evaluation['friendly_name'] = self.evaluate_friendly_name()
+        evaluation['metadata'] = self.evaluate_metadata()
         evaluation['container'] = self.evaluate_container()
+        evaluation['translation'] = self.evaluate_translation()
 
         return evaluation
 
     def evaluate_friendly_name(self) -> Union[str, bool]:
         if self.friendly_name() is None:
             return "Missing friendly name"
+
+        return True
+
+    def evaluate_metadata(self) -> Union[str, bool]:
+        if isinstance(self.metadata, ToolMetadata):
+            required = {
+                "contributors": self.metadata.contributors,
+                "created date": self.metadata.dateCreated,
+                "updated date": self.metadata.dateUpdated,
+                "institution": self.metadata.institution,
+            }
+
+            missing = []
+            for key, field in required.items():
+                if field is None or not field:
+                    missing.append(key)
+
+            if missing:
+                return f"Missing metadata: {', '.join(missing)}"
+        # elif isinstance(self.metadata, ...):
+        else:
+            return "Incorrect metadata class"
 
         return True
 
@@ -42,6 +73,19 @@ class BioinformaticsTool(CommandTool, ABC):
             return f"image {self.container()} not found"
         except Exception as e:
             return f"image {self.container()}: {str(e)}"
+
+        return True
+
+    def evaluate_translation(self):
+        cwl_file_path = f"/tmp/janis/tests/{self.id()}/cwl"
+        wdl_file_path = f"/tmp/janis/tests/{self.id()}/wdl"
+
+        self.translate(SupportedTranslation.CWL, to_console=False, to_disk=True, export_path=cwl_file_path)
+        self.translate(SupportedTranslation.WDL, to_console=False, to_disk=True, export_path=wdl_file_path)
+
+        # TODO: translate and validate
+        CwlTranslator.validate_command_for(cwl_file_path, "", "", "")
+        WdlTranslator.validate_command_for(wdl_file_path, "", "", "")
 
         return True
 
