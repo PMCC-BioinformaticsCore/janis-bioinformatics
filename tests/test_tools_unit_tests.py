0b5edecd595a8281dba2c67aebd58efa8d8904ba
Juny Kesumadewi <jkesumadewi@5000L-152848-M.local>
2020-10-21 17:18:40 +1100

    Cover both cromwell and cwltool in testing tool test suite

--- tests/test_tools_unit_tests.py
+++ tests/test_tools_unit_tests.py
@@ -1,15 +1,17 @@
 import unittest
-import janis_bioinformatics
 from parameterized import parameterized
+
+import janis_bioinformatics
 
 from janis_core.tool.test_suite_runner import ToolTestSuiteRunner
 from janis_core.tool import test_helpers as test_helper
 
 all_tools = test_helper.get_all_tools([janis_bioinformatics.tools])
+
 all_versioned_tools = []
 # TODO: revert to full list
 # for tool_versions in all_tools:
-for tool_versions in all_tools[148:152]:
+for tool_versions in all_tools[150:152]:
     for versioned_tool in tool_versions:
         all_versioned_tools.append(versioned_tool)
 
@@ -28,16 +30,33 @@ class RunAllToolsTestSuite(unittest.TestCase):
             self.fail(error_message)
         else:
             runner = ToolTestSuiteRunner(tool)
+            fail_count_by_engine = {}
 
             for tc in tool.tests():
-                with self.subTest(name=str(tc.name)):
-                    failed, succeeded = runner.run_one_test_case(tc)
+                test_results = runner.run_one_test_case(tc)
 
-                if len(failed) > 0:
-                    self.failed_tools[name] = f"{len(failed)} test case(s) failed"
-                    self.fail("; ".join(failed))
+                for engine in test_results:
+                    failed, succeeded = test_results[engine]
+
+                    if engine not in fail_count_by_engine:
+                        fail_count_by_engine[engine] = 0
+
+                    if len(failed) > 0:
+                        fail_count_by_engine[engine] += 1
+
+            error_messages = []
+            for engine in fail_count_by_engine:
+                fail_count = fail_count_by_engine[engine]
+                name_on_report = f"{name} - {engine}"
+                if fail_count > 0:
+                    self.failed_tools[name_on_report] = f"{fail_count} test case(s) failed"
+                    error_messages.append(f"{engine}: {'; ' .join(failed)}")
                 else:
-                    self.succeeded_tools.add(name)
+                    self.succeeded_tools.add(name_on_report)
+
+            if error_messages:
+                self.fail("| ".join(error_messages))
 
     def test_report(self):
         test_helper.print_test_report(failed=self.failed_tools, succeeded=self.succeeded_tools)
+
