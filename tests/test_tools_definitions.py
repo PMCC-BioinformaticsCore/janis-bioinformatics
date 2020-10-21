0731bf7387dfa4f4f899ae936a68cec15b50ab8f
Juny Kesumadewi <jkesumadewi@5000L-152848-M.local>
2020-10-21 17:13:08 +1100

    Make each tool one unit test case

--- tests/test_tools_definitions.py
+++ tests/test_tools_definitions.py
@@ -1,31 +1,41 @@
 import unittest
-import janis_bioinformatics
+from parameterized import parameterized
 
+import janis_bioinformatics
 from janis_core.tool.test_definitions import ToolEvaluator
-from janis_core.tool import test_helpers
+from janis_core.tool import test_helpers as test_helper
+
+all_tools = test_helper.get_all_tools([janis_bioinformatics.tools])
+
+all_versioned_tools = []
+# TODO: revert to full list
+for tool_versions in all_tools:
+# for tool_versions in all_tools[148:152]:
+    for versioned_tool in tool_versions:
+        all_versioned_tools.append(versioned_tool)
 
 
 class TestToolsDefinitions(unittest.TestCase):
-    def test_all_tools(self):
-        all_tools = test_helpers.get_all_tools([janis_bioinformatics.tools])
+    failed = {}
+    succeeded = set()
 
-        failed = {}
-        succeeded = set()
-        # TODO: revert to full list
-        # for tool_versions in all_tools:
-        for tool_versions in all_tools[132:134]:
-            for versioned_tool in tool_versions:
-                evaluation = ToolEvaluator.evaluate(versioned_tool)
+    @parameterized.expand([
+        [t.id(), t] for t in all_versioned_tools
+    ])
+    def test_all_tools(self, name, tool):
+        evaluation = ToolEvaluator.evaluate(tool)
 
-                if evaluation is True:
-                    succeeded.add(versioned_tool.versioned_id())
-                else:
-                    failed[versioned_tool.versioned_id()] = evaluation
+        if evaluation is True:
+            self.succeeded.add(tool.versioned_id())
+        else:
+            self.failed[tool.versioned_id()] = evaluation
+            raise Exception(evaluation)
 
-        test_helpers.print_test_report(failed, succeeded)
+    def test_report(self):
+        test_helper.print_test_report(failed=self.failed, succeeded=self.succeeded)
 
-        if len(failed) > 0:
+        if (len(self.failed) > 0):
             raise Exception(
-                f"There were {len(failed)} tool(s) that did not contain sufficient metadata to include in the "
+                f"There were {len(self.failed)} tool(s) that did not contain sufficient metadata to include in the "
                 f"janis_* repository. Please check to ensure your tool is in the list below"
             )
