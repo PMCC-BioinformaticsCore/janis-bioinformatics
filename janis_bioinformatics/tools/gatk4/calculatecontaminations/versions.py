a5c09bfbcda1762e8fd4a5f75276bc0369165bd1
SHollizeck <sebastian.hollizeck@gmx.de>
2020-11-29 16:13:57 +1100

    update all the other tool version

--- janis_bioinformatics/tools/gatk4/calculatecontaminations/versions.py
+++ janis_bioinformatics/tools/gatk4/calculatecontaminations/versions.py
@@ -1,5 +1,12 @@
 from .base import Gatk4CalculateContaminationBase
-from ..versions import Gatk_4_1_2_0, Gatk_4_1_3_0, Gatk_4_1_4_0
+from ..versions import (
+    Gatk_4_1_2_0,
+    Gatk_4_1_3_0,
+    Gatk_4_1_4_0,
+    Gatk_4_1_6_0,
+    Gatk_4_1_7_0,
+    Gatk_4_1_8_0,
+)
 
 
 class Gatk4CalculateContamination_4_1_2(Gatk_4_1_2_0, Gatk4CalculateContaminationBase):
@@ -14,7 +21,19 @@ class Gatk4CalculateContamination_4_1_4(Gatk_4_1_4_0, Gatk4CalculateContaminatio
     pass
 
 
-Gatk4CalculateContaminationLatest = Gatk4CalculateContamination_4_1_4
+class Gatk4CalculateContamination_4_1_6(Gatk_4_1_6_0, Gatk4CalculateContaminationBase):
+    pass
+
+
+class Gatk4CalculateContamination_4_1_7(Gatk_4_1_7_0, Gatk4CalculateContaminationBase):
+    pass
+
+
+class Gatk4CalculateContamination_4_1_8(Gatk_4_1_8_0, Gatk4CalculateContaminationBase):
+    pass
+
+
+Gatk4CalculateContaminationLatest = Gatk4CalculateContamination_4_1_8
 
 if __name__ == "__main__":
     print(Gatk4CalculateContaminationBase().help())
