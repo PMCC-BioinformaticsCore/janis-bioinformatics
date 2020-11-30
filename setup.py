64232a71a0244c15e7fe1cbdbd6dea1628b9b3ba
Juny Kesumadewi <juny.kesumadewi@unimelb.edu.au>
2020-11-30 16:36:02 +1100

    Add package data

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         "janis_bioinformatics." + p
         for p in sorted(find_packages("./janis_bioinformatics"))
     ],
+    package_data={"janis_bioinformatics": ["*/test_data/*"]},
     include_package_data=True,
     entry_points={
         "janis.extension": ["bioinformatics=janis_bioinformatics"],
