b8fa16bcb8102ad94e1e4375994bde04d4f6e364
Jiaan Yu <jiaan7.yu@gmail.com>
2021-02-24 17:16:59 +1100

    Change to facets basecommand

--- janis_bioinformatics/tools/facets/__init__.py
+++ janis_bioinformatics/tools/facets/__init__.py
@@ -1,4 +1,4 @@
 from .snp_pileup.versions import (
-    FacetsSnpPileup_0_5_14,
+    FacetsSnpPileup_0_5_14_2,
     FacetsSnpPileupLatest,
 )
---
+++ janis_bioinformatics/tools/facets/versions.py
@@ -0,0 +1,9 @@
+from abc import ABC
+
+
+class Facets_0_5_14_2(ABC):
+    def container(self):
+        return "vanallenlab/facets:v0.5.14-2"
+
+    def version(self):
+        return "0.5.14-2"
