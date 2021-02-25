40eb8fa58b98f086af18cb5fc3aa4c53a900cfc0
Jiaan Yu <jiaan7.yu@gmail.com>
2021-02-25 14:27:13 +1100

    Add facets_plot

---
+++ janis_bioinformatics/tools/facets/facets_plot/versions.py
@@ -0,0 +1,9 @@
+from .base import FacetsPlotBase
+from ..versions import Facets_0_5_14_2
+
+
+class FacetsPlot_0_5_14_2(Facets_0_5_14_2, FacetsPlotBase):
+    pass
+
+
+FacetsPlotLatest = FacetsPlot_0_5_14_2
