67a1c61028269a270bac0d607b9499b07833e0fe
Jiaan Yu <jiaan7.yu@gmail.com>
2021-04-30 17:25:07 +1000

    Clean up oncopipe

--- janis_bioinformatics/tools/oshlack/__init__.py
+++ janis_bioinformatics/tools/oshlack/__init__.py
@@ -1,3 +1,2 @@
-# from .oncopipe.oncopipe import OncopipeWorkflow, OncopipeSamplePreparation
 from .oncopipe.oncopipeSample import OncopipeSamplePreparation
 from .oncopipe.oncopipe import OncopipeWorkflow
