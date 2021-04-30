d6ddf40ca486a34037ae62ef930e2c31a27d5813
Jiaan Yu <jiaan7.yu@gmail.com>
2021-04-30 17:14:24 +1000

    Import oncopipe

--- janis_bioinformatics/tools/oshlack/__init__.py
+++ janis_bioinformatics/tools/oshlack/__init__.py
@@ -1,2 +1,3 @@
 # from .oncopipe.oncopipe import OncopipeWorkflow, OncopipeSamplePreparation
 from .oncopipe.oncopipeSample import OncopipeSamplePreparation
+from .oncopipe.oncopipe import OncopipeWorkflow
