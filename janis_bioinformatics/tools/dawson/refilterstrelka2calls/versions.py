62c365461d0fe745bc8711aa55746cf3c5af2dfe
Sebastian Hollizeck <sebasian.hollizeck@petermac.org>
2021-10-12 16:02:32 +1100

    fixed an import typo

--- janis_bioinformatics/tools/dawson/refilterstrelka2calls/versions.py
+++ janis_bioinformatics/tools/dawson/refilterstrelka2calls/versions.py
@@ -6,4 +6,4 @@ class RefilterStrelka2Calls_0_1(RefilterStrelka2CallsBase, DawsonToolkit_0_2):
     pass
 
 
-RefilterStrelka2CallsLatest = RefilterStrelka2Calls_0_2
+RefilterStrelka2CallsLatest = RefilterStrelka2Calls_0_1
