592713e314c2f9fae40dcd2c922a5693eb288085
Sebastian Hollizeck <sebasian.hollizeck@petermac.org>
2021-10-12 15:55:39 +1100

    fixed a typo and migrated to newest docker container

--- janis_bioinformatics/tools/dawson/callsomaticfreebayes/versions.py
+++ janis_bioinformatics/tools/dawson/callsomaticfreebayes/versions.py
@@ -1,8 +1,8 @@
 from .base import CallSomaticFreeBayesBase
-from ..versions import DawsonToolkit_0_1
+from ..versions import DawsonToolkit_0_2
 
 
-class CallSomaticFreeBayes_0_1(CallSomaticFreeBayesBase, DawsonToolkit_0_1):
+class CallSomaticFreeBayes_0_1(CallSomaticFreeBayesBase, DawsonToolkit_0_2):
     pass
 
 
--- janis_bioinformatics/tools/dawson/refilterstrelka2calls/versions.py
+++ janis_bioinformatics/tools/dawson/refilterstrelka2calls/versions.py
@@ -1,9 +1,9 @@
 from .base import RefilterStrelka2CallsBase
-from ..versions import DawsonToolkit_0_1
+from ..versions import DawsonToolkit_0_2
 
 
-class RefilterStrelka2Calls_0_1(RefilterStrelka2CallsBase, DawsonToolkit_0_1):
+class RefilterStrelka2Calls_0_1(RefilterStrelka2CallsBase, DawsonToolkit_0_2):
     pass
 
 
-RefilterStrelka2CallsLatest = RefilterStrelka2Calls_0_1
+RefilterStrelka2CallsLatest = RefilterStrelka2Calls_0_2
