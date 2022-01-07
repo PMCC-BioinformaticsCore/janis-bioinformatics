9528fddd4547bce817e61ba65892a7abbf418ed2
Jiaan Yu <jiaan7.yu@gmail.com>
2022-01-07 12:26:21 +1100

    Remove unused workflows

--- janis_bioinformatics/tools/pmac/__init__.py
+++ janis_bioinformatics/tools/pmac/__init__.py
@@ -11,8 +11,6 @@ from .performanceSummaryTargetedWorkflow import PerformanceSummaryTargeted_0_1_0
 from .performanceSummaryGenomeWorkflow import PerformanceSummaryGenome_0_1_0
 from .addBamStatsSomaticWorkflow import AddBamStatsSomatic_0_1_0
 from .addBamStatsGermlineWorkflow import AddBamStatsGermline_0_1_0
-from .molpathGermlineWorkflow import MolpathGermline_1_0_0
-from .molpathTumorOnlyWorkflow import MolpathTumorOnly_1_0_0
 from .generatevardictheaderlines import GenerateVardictHeaderLines
 from .generatebedtoolscoveragegenomefile import GenerateGenomeFileForBedtoolsCoverage
 from .generateintervalsbychromosome.generateintervalsbychromosome import (
