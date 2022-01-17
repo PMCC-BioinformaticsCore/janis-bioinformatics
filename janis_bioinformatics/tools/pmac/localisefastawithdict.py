30cf34cc3d782f73ef5236fe6f320f110475e735
Jiaan Yu <jiaan7.yu@gmail.com>
2022-01-17 12:03:19 +1100

    Add LocaliseFastaWithDict

---
+++ janis_bioinformatics/tools/pmac/localisefastawithdict.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+from distutils import extension
+from turtle import position
+from janis_core import ToolInput, ToolOutput, ToolArgument, InputSelector, Filename
+
+from janis_unix.tools.unixtool import UnixTool
+from janis_bioinformatics.data_types import FastaWithDict
+
+
+class LocaliseFastaWithDict(UnixTool):
+    def tool(self):
+        return "LocaliseFastaWithDict"
+
+    def friendly_name(self):
+        return "LocaliseFastaWithDict"
+
+    def base_command(self):
+        return None
+
+    def inputs(self):
+        return [
+            ToolInput(
+                "reference",
+                FastaWithDict,
+            ),
+            ToolInput(
+                "reference_all",
+                Filename(prefix=InputSelector("reference"), extension="*"),
+                position=2,
+            ),
+            ToolInput(
+                "reference_output",
+                Filename(prefix=InputSelector("reference"), extension=".fasta"),
+            ),
+        ]
+
+    def outputs(self):
+        return [
+            ToolOutput("out", FastaWithDict, selector=InputSelector("reference_output"))
+        ]
+
+    def arguments(self):
+        return [
+            ToolArgument("cp", position=1, shell_quote=False),
+            ToolArgument(".", position=3, shell_quote=False),
+        ]
+
+    def tool_provider(self):
+        return "Peter MacCallum Cancer Centre"
+
+    def version(self):
+        return "v0.1.0"
+
+    def bind_metadata(self):
+        self.metadata.dateCreated = datetime(2022, 1, 7)
+        self.metadata.dateUpdated = datetime(2022, 1, 7)
+        self.metadata.contributors = ["Jiaan Yu"]
+        self.metadata.documentation = """\
+Localise Arrary of FastqGZ pairs
+        """
