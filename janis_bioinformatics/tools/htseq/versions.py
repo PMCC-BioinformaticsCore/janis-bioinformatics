4e0030dfe76a049764854d67da9f66512c07edbf
Jiaan Yu <jiaan7.yu@gmail.com>
2022-01-20 16:16:37 +1100

    Fix container link

--- janis_bioinformatics/tools/htseq/versions.py
+++ janis_bioinformatics/tools/htseq/versions.py
@@ -3,7 +3,7 @@ from abc import ABC
 
 class HTSeq_1_99_2(ABC):
     def container(self):
-        return "biocontainers/htseq:1.99.2--py39haf81c86_0"
+        return "quay.io/biocontainers/htseq:1.99.2--py39haf81c86_0"
 
     def version(self):
         return "1.99.2"
